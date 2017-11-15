e1dfcfd9204277f227365772c399cb336b2e9be7
corgisout <33465193+corgisout@users.noreply.github.com>
2017-11-15 17:01:11 +0100

    fixa onödig kod som kommit in i theme

--- config/theme.php
+++ config/theme.php
@@ -21,14 +21,14 @@ return [
             "favicon"       => "img/favicon/favicon_256x256.png",
 
             // Style and stylesheets
-<<<<<<< HEAD
+
             "stylesheets" => ["css/style.min.css"],
-=======
+
             "stylesheets" => [
                 "css/style.min.css",
                 "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
         ],
->>>>>>> b597d1b9f700e06e6c56c6af2a9951e5a6ee5d5e
+
             "styleInline" => null,
 
             // JavaScript
